ae6fe7ac68bff8406cd96357f804bdda5ec9917c
suryaOct2024 <surya.psgtech@gmail.com>
2024-10-15 14:47:45 -0500

    Add files via upload

---
+++ HomeAssignments/Chrome.java
@@ -0,0 +1,21 @@
+package week1.day1.HomeAssignments;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 1998;
+		char shortcutKey = 'C';
+		
+		System.out.println("Version: "+version);
+		System.out.println("developer is : "+developer);
+		System.out.println("Is it beta version? "+isBeta);
+		System.out.println("Release Year: "+releaseYear);
+		System.out.println("Shortcut key: "+shortcutKey);
+
+	}
+
+}
